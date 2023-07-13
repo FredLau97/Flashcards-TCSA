f5046b5e243709dc239e5fac11560c6944ed2c58
Frederik Lau Rasmussen <frederik-lau@live.dk>
2023-07-13 20:29:17 +0200

    Created FlashcardDTO class

---
+++ Flashcards/FlashcardDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flashcards
+{
+    internal class FlashcardDTO
+    {
+        public int FlashcardID { get; set; }
+        public string CardFront { get; set; }
+        public string CardBack { get; set; }
+
+        public FlashcardDTO() { }
+
+        public FlashcardDTO(int flashcardID, string cardFront, string cardBack)
+        {
+            FlashcardID = flashcardID;
+            CardFront = cardFront;
+            CardBack = cardBack;
+        }
+    }
+}
