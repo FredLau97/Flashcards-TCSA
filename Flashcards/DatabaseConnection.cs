fb7176074ddd7b7fbfeaf3aedc5b5ca73a8a7acc
Frederik Lau Rasmussen <frederik-lau@live.dk>
2023-07-13 13:11:41 +0200

    Added DatabaseConnection class, and implemented Open- and Close connection methods

---
+++ Flashcards/DatabaseConnection.cs
@@ -0,0 +1,65 @@
+﻿using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Flashcards
+{
+    internal class DatabaseConnection
+    {
+        private SqlConnection _connection;
+        private SqlCommand _cmd;
+        private SqlDataAdapter _adapter;
+
+        public DatabaseConnection()
+        {
+            _connection = new(ConfigurationManager.ConnectionStrings["Flashcards"].ConnectionString);
+            _cmd = new();
+            _adapter = new();
+        }
+
+        // Opens the connection to the database
+        // Checks if the connection is already open, and if not, tries to open it
+        // Returns true if the connection was openend successfully, otherwise returns false
+        // If there was an error, it will be logged to the console
+        public bool OpenConnection()
+        {
+            try
+            {
+                if (_connection.State == ConnectionState.Closed)
+                {
+                    _connection.Open();
+                    Console.WriteLine("Connection to database established");
+                    return true;
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine($"Connection to database failed...\n" +
+                    $"Error: {e.Message}");
+            }
+
+            return false;
+        }
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                if (_connection.State == ConnectionState.Open)
+                {
+                    _connection.Close();
+                    Console.WriteLine("Connection to database closed");
+                    return true;
+                }
+            }
+            catch (SqlException e)
+            {
+                Console.WriteLine($"Connection to database failed to close...\n" +
+                                       $"Error: {e.Message}");
+            }
+
+            return false;
+        }
+    }
+}
--- Flashcards/Menu.cs
+++ Flashcards/Menu.cs
@@ -8,5 +8,24 @@ namespace Flashcards
 {
     internal class Menu
     {
+        private InputHandler _inputHandler;
+
+        public Menu() 
+        {
+            _inputHandler = new();
+
+            Console.WriteLine("Welcome to the Flashcards App!");
+            Show();
+        }
+
+        public void Show()
+        {
+            Console.WriteLine("---------------");
+            Console.WriteLine("1. Add a new flashcard");
+            Console.WriteLine("2. Show all flashcards");
+            Console.WriteLine("3. Practice");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("---------------");
+        }
     }
 }
